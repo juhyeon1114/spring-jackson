ebcbb208d67f3336edbeeeacf0b57d48b4827fc4
juhyeon1114 <juhyeon1114@gmail.com>
2024-07-19 11:42:11 +0900

    init

---
+++ src/main/java/study/springjackson/SpringjacksonApplication.java
@@ -0,0 +1,13 @@
+package study.springjackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjacksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjacksonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/springjackson/SpringjacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springjackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjacksonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
