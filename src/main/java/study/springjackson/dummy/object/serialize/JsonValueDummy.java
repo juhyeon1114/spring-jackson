e5ce4ed06c11d518d720d3d42586c6d4b9b6e5d9
juhyeon1114 <juhyeon1114@gmail.com>
2024-07-23 14:40:47 +0900

    JsonValue 개선

--- src/main/java/study/springjackson/dummy/object/serialize/JsonValueDummy.java
+++ src/main/java/study/springjackson/dummy/object/serialize/JsonValueDummy.java
@@ -1,16 +1,24 @@
 package study.springjackson.dummy.object.serialize;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.HashMap;
 
 public class JsonValueDummy {
 
     public String id = "id1";
-    @JsonValue
     public Person person = new Person();
 
     public class Person {
         public String name = "name2";
         public int age = 30;
+    }
+
+    @JsonValue
+    public HashMap<String, String> jsonResult() {
+        HashMap<String, String> result = new HashMap<>();
+        result.put("id", id);
+        result.put("name", person.name);
+        return result;
     }
 
 }
